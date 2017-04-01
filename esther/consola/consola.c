1b732275c9557c7bc0147a9ae80f068d1bc6fba7
Nachito <igfernandezsoto@gmail.com>
2017-04-01 18:16:14 -0300

    de algun lado se empieza...

---
+++ esther/consola/consola.c
@@ -0,0 +1,19 @@
+/*
+ * consola.c
+ *
+ *  Created on: 1/4/2017
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main() {
+	printf("%s", "holaa");
+	return 1;
+}
