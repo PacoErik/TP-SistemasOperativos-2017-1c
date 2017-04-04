d8a2dbe22f8128214ecd84694563a66cbb171a7b
Nachito <igfernandezsoto@gmail.com>
2017-04-03 21:07:57 -0300

    A gonza no le funca Kernel JAJ

--- esther/kernel/kernel.c
+++ esther/kernel/kernel.c
@@ -7,45 +7,6 @@
 
 #include "operadores_header_serializador_handshake.h"
 
-void handshake(int socket, char operacion) {
-	printf("Conectando a servidor 0 porciento\n");
-	/*	printf("Conectando a servidor 0 porciento\n");
-	 printf("Conectando a servidor 23 porciento\n");
-	 sleep(1);
-	 printf("Conectando a servidor 55 porciento\n");
-	 printf("Conectando a servidor 84 porciento\n");
-	 sleep(2);*/
-	struct headerDeLosRipeados handy;
-	handy.bytesDePayload = 0;
-	handy.codigoDeOperacion = operacion;
-
-	int buffersize = sizeof(struct headerDeLosRipeados);
-	// int buffersize = sizeof(struct headerDeLosRipeados) + 1;
-	char *buffer = malloc(buffersize);
-	//printf("%i\n", buffersize);
-	//buffer = malloc(buffersize);
-
-	//serializarHeader(handy,&buffer);
-	//buffer[buffersize] = '\0';
-	serializarHeader(&handy, buffer);
-	//printf("%s\n", buffer);
-	send(socket, (void*) buffer, buffersize, 0);
-
-	// Recibe lo que responde el servidor
-	char respuesta[1024];
-	int bytesRecibidos = recv(socket, (void *) &respuesta, sizeof(respuesta), 0);
-
-	if (bytesRecibidos > 0) {
-		printf("Conectado a servidor 100 porciento\n");
-		printf("Mensaje del servidor: \"%s\"\n", respuesta);
-	}
-	else {
-		printf("Ripeaste\n");
-		exit(0);
-	}
-	free(buffer);
-}
-
 int main(void) {
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
