6993511fb7a7fb502a7e6a201ab627306e02453c
PacoErik <erik@paco.16mb.com>
2017-04-05 16:31:04 -0300

    Consolacarga la configuración desde un archivo y logea varios pasos realizados

--- esther/consola/consola.c
+++ esther/consola/consola.c
@@ -6,12 +6,75 @@
  */
 
 #include "operadores_header_serializador_handshake.h"
+#include "commons/log.h"
+#include "commons/config.h"
+#include "commons/string.h"
 
-int servidor;
+#define RUTA_CONFIG "./config.cfg"
+#define RUTA_LOG "./consola.log"
 
-/*
- * ↓ PARTE GRAFICA Y SUS FUNCIONES ↓
- */
+int servidor; //kernel
+char IP_KERNEL[16]; // 255.255.255.255 = 15 caracteres + 1 ('\0')
+int PUERTO_KERNEL;
+t_log* log;
+t_config* config;
+
+void imprimirOpcionesDeConsola();
+void iniciarPrograma();
+void desconectarPrograma();
+void desconectarConsola();
+void enviarMensaje();
+void leerMensaje();
+void limpiarPantalla();
+void interaccionConsola();
+
+void establecerConfiguracion() {
+	if(config_has_property(config, "PUERTO_KERNEL")){
+		int port = config_get_int_value(config, "PUERTO_KERNEL");
+		PUERTO_KERNEL = port;
+		printf("\nPuerto Kernel: %d \n",PUERTO_KERNEL);
+	}else{
+		log_error(log, "Error al leer el puerto del Kernel");
+	}
+	if(config_has_property(config, "IP_KERNEL")){
+		strcpy(IP_KERNEL,config_get_string_value(config, "IP_KERNEL"));
+		printf("IP Kernel: %s \n",IP_KERNEL);
+	}else{
+		log_error(log, "Error al leer la IP del Kernel");
+	}
+}
+
+void configurar(char *path_File, char *path_log) {
+	log = log_create(RUTA_LOG, "consola",true, LOG_LEVEL_INFO);
+	if(config_keys_amount(config = config_create(path_File)) > 0) {
+		establecerConfiguracion();
+	} else {
+		log_error(log, "Error al leer archivo de configuración");
+	}
+	config_destroy(config);
+
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+	direccionServidor.sin_port = htons(8081);
+	servidor = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect(servidor, (struct sockaddr *) &direccionServidor,sizeof(direccionServidor)) < 0) {
+		close(servidor);
+		log_info(log,"No se pudo conectar al Kernel");
+		exit(0);
+	}
+	log_info(log,"Conectado al Kernel");
+}
+
+
+
+int main(void) {
+	configurar(RUTA_CONFIG, RUTA_LOG);
+	handshake(servidor, CONSOLA);
+	interaccionConsola();
+
+	return 0;
+}
 
 void imprimirOpcionesDeConsola() {
 	printf("\n--------------------\n");
@@ -39,7 +102,6 @@ void desconectarPrograma() {
 
 void desconectarConsola() {
 	close(servidor);
-	printf("Hasta la vuelta!\n");
 	exit(EXIT_SUCCESS);
 }
 
@@ -68,7 +130,7 @@ void leerMensaje() {
 	mensaje[bytesRecibidos]='\0';
 	if(bytesRecibidos <= 0){
 		close(servidor);
-		printf("Servidor desconectado\n");
+		log_info(log,"Servidor desconectado luego de intentar leer mensaje");
 		exit(0);
 	}
 	printf("\nMensaje recibido: %s\n",mensaje);
@@ -99,88 +161,38 @@ void interaccionConsola() {
 		char opcion = input[0];
 
 		switch(opcion) {
-		case '1' : {
-			iniciarPrograma(); // TODO
-			break;
-		}
-		case '2' : {
-			desconectarPrograma(); // TODO
-			break;
-		}
-		case '3' : {
-			desconectarConsola();
-			break;
-		}
-		case '4' : {
-			enviarMensaje();
-			leerMensaje();
-			break;
-		}
-		case '5' : {
-			limpiarPantalla();
-			break;
-		}
-		case '6' : {
-			imprimirOpcionesDeConsola();
-			break;
-		}
-		default : {
-			printf("\nColoque una opcion correcta (1, 2, 3, 4, 5 o 6)\n");
-		}
+			case '1' : {
+				iniciarPrograma(); // TODO
+				log_info(log,"Comando de inicio de programa ejecutado");
+				break;
+			}
+			case '2' : {
+				desconectarPrograma(); // TODO
+				log_info(log,"Comando de desconexión de programa ejecutado");
+				break;
+			}
+			case '3' : {
+				log_info(log,"Comando de apagado de consola ejecutado");
+				desconectarConsola();
+				break;
+			}
+			case '4' : {
+				enviarMensaje();
+				leerMensaje();
+				log_info(log,"Comando de envío de mensaje ejecutado");
+				break;
+			}
+			case '5' : {
+				limpiarPantalla();
+				break;
+			}
+			case '6' : {
+				imprimirOpcionesDeConsola();
+				break;
+			}
+			default : {
+				printf("\nColoque una opcion correcta (1, 2, 3, 4, 5 o 6)\n");
+			}
 		}
 	}
-}
-
-/*
- * ↑ PARTE GRAFICA Y SUS FUNCIONES ↑
- */
-
-int main(void) {
-
-	struct sockaddr_in direccionServidor;
-	direccionServidor.sin_family = AF_INET;
-	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
-	direccionServidor.sin_port = htons(8081);
-
-	servidor = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (connect(servidor, (struct sockaddr *) &direccionServidor,sizeof(direccionServidor))) {
-		printf("No se pudo conectar al servidor\n");
-		return 1;
-	}
-
-	handshake(servidor, CONSOLA);
-
-/*
-
-	int buffersize = 256;
-	char buffer[buffersize];
-	int i;
-	for (i = 0; i < buffersize; i++) {
-		buffer[i] = '\0';
-	}
-
-	while (1) {
-		printf("\nEscribir mensaje: ");
-		scanf("%s", buffer);
-
-		if (send(servidor, buffer, strlen(buffer), 0) < 0) {
-			printf("\nNo se pudo enviar el mensaje\n");
-			return 1;
-		}
-
-		if (recv(servidor, buffer, buffersize, 0) < 0) {
-			printf("\nSe terminó la conexión\n");
-			return 1;
-		}
-
-		buffer[buffersize] = '\0';
-		printf("\nMensaje del servidor: %s", buffer);
-	}
-
-*/
-
-	interaccionConsola();
-
-	return 0;
 }
