904fdb1ea88e1028a304058eb0fdc42bc0f66a7d
Fernando <wzfernandochen@gmail.com>
2017-07-04 00:31:03 -0300

    [Kernel: Capa de Memoria] Pequenio fix

--- esther/kernel/capamem.c
+++ esther/kernel/capamem.c
@@ -64,7 +64,7 @@ int alocar_bloque_en_pagina(int PID, int nro_pagina, int size) {
 							: -2;
 			}
 
-			if (hm_libre.size > size + sizeof(HeapMetadata)) {
+			if (hm_libre.size >= size + sizeof(HeapMetadata)) {
 				return escribir_heap_metadata(PID, nro_pagina, offset, size, hm_libre.size)
 							? _direccion_pag_offset(nro_pagina, offset + sizeof(HeapMetadata))
 							: -2;
